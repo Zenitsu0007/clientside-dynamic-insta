efe5f2921c419b17917d6bdc07711dcdf49c40e0
Zejia Shen <jasnshen@umich.edu>
2024-02-18 17:56:16 -0500

    index api

---
+++ insta485/api/index.py
@@ -0,0 +1,13 @@
+import flask
+import insta485
+
+@insta485.app.route("/api/v1/", methods=["GET"])
+def get_index():
+    """Return API resource URLs."""
+    context = {
+        "comments": "/api/v1/comments/",
+        "likes": "/api/v1/likes/",
+        "posts": "/api/v1/posts/",
+        "url": "/api/v1/"
+    }
+    return flask.jsonify(**context), 200
