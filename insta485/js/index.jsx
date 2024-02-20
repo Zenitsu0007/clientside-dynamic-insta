53f612d652392b06be8d17fdd3fe3c475570dd97
shihuis <shihuis@umich.edu>
2024-02-19 20:46:09 -0500

    first submit

--- insta485/js/index.jsx
+++ insta485/js/index.jsx
@@ -4,44 +4,49 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "./post";
 
 export default function Index({ url }) {
-    window.history.scrollRestoration = "manual";
-    // Initialize the next url and posts lists (results list in json file)
-    const [next, setNext] = useState(url);
-    const [posts, setPosts] = useState([]);
-    const fetchNext = () => {
-        // For initialize
-        const fetchUrl = posts.length > 0 ? next : url;
-        fetch(fetchUrl, { credentials: "same-origin" })
-        .then(response => {
-            if (!response.ok) throw Error(response.statusText);
-            return response.json();
-        })
-        .then(data => {
-            setNext(data.next);
-            setPosts([...posts, ...data.results]);
-        })
-        .catch(error => console.log(error));
-    };
+  window.history.scrollRestoration = "manual";
+  // Initialize the next url and posts lists (results list in json file)
+  const [next, setNext] = useState(url);
+  const [posts, setPosts] = useState([]);
+  const fetchNext = () => {
+    // For initialize
+    const fetchUrl = posts.length > 0 ? next : url;
+    fetch(fetchUrl, { credentials: "same-origin" })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        setNext(data.next);
+        setPosts([...posts, ...data.results]);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    if (posts.length === 0) fetchNext();
+  if (posts.length === 0) fetchNext();
 
-    return (
-        <div className="scrollableDiv" style={{ overflow: 'auto' }}>
-            <InfiniteScroll
-                dataLength={posts.length}
-                hasMore={!!next && next !== ""}
-                next={fetchNext}
-                loader={next !== null && next !== "" ? 
-                <h4>Loading...</h4> : 
-                <h4>No more posts</h4>}>
-                    {posts.map((result) => (
-                        <div key={result.postid}>
-                            <Post url={result.url} postid={result.postid}/>
-                        </div>
-                    ))}
-            </InfiniteScroll>
-        </div>
-    );
+  return (
+    <div className="scrollableDiv" style={{ overflow: "auto" }}>
+      <InfiniteScroll
+        dataLength={posts.length}
+        hasMore={!!next && next !== ""}
+        next={fetchNext}
+        loader={
+          next !== null && next !== "" ? (
+            <h4>Loading...</h4>
+          ) : (
+            <h4>No more posts</h4>
+          )
+        }
+      >
+        {posts.map((result) => (
+          <div key={result.postid}>
+            <Post url={result.url} postid={result.postid} />
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
 }
 
 Index.propTypes = {
--- insta485/js/likes.jsx
+++ insta485/js/likes.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+export default function Likes({ likes, handleLike }) {
+  // Button text changes based on whether the user likes the post or not
 
-export default function Likes({ likes, handleLike}) {
-    // Button text changes based on whether the user likes the post or not
+  const buttonText = likes.lognameLikesThis ? "Unlike" : "Like";
 
-    const buttonText = likes.lognameLikesThis ? 'Unlike' : 'Like';
-
-    return (
-        <div> 
-            <button onClick={handleLike} data-testid="like-unlike-button" type="button">
-                {buttonText}
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        onClick={handleLike}
+        data-testid="like-unlike-button"
+        type="button"
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
 }
 
 Likes.propTypes = {
-    likes: PropTypes.shape({
-        lognameLikesThis: PropTypes.bool,
-        numLikes: PropTypes.number,
-        url:PropTypes.string,
-    }).isRequired,
-    handleLike: PropTypes.func.isRequired,
+  likes: PropTypes.shape({
+    lognameLikesThis: PropTypes.bool,
+    numLikes: PropTypes.number,
+    url: PropTypes.string,
+  }).isRequired,
+  handleLike: PropTypes.func.isRequired,
 };
